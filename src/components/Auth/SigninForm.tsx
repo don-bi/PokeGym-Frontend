b2b16cc353c39399311ffc9aee61d89fda167bd6
Donald Bi <donb14229@gmail.com>
2024-09-22 23:26:08 -0400

    Made the sign in form

--- src/components/Auth/SigninForm.tsx
+++ src/components/Auth/SigninForm.tsx
@@ -1,22 +1,35 @@
 import { GoogleLogin } from "@react-oauth/google";
 import { googleSignIn } from "../../services/authentication";
 import { useAppDispatch } from "../../app/hooks";
+import './SignInForm.css';
 
 
 export default function SignInForm() {
     const dispatch = useAppDispatch();
 
     return (
-        <div>
-            <GoogleLogin
-            onSuccess={(credentialResponse) => {
-                googleSignIn(dispatch, credentialResponse);
-            }}
+        <main className="eevee flex items-center justify-center">
+            
+            <div className="middle-container flex flex-col w-3/12 gap-10 items-center backdrop-blur-sm h-4/6 shadow-slate-400 rounded-lg px-10 py-6">
+                <div className="flex items-center">
+                    <img src="/pokeball-icon.png" alt="" className="w-20"/>
+                    <h1 className="text-4xl font-bold text-slate-200 logo-text">PokeGym</h1>
+                </div>
 
-            onError={() => {
-                console.log("Error");
-            }}
-            />
-        </div>
+                <GoogleLogin
+                onSuccess={(credentialResponse) => {
+                    googleSignIn(dispatch, credentialResponse);
+                }}
+
+                onError={() => {
+                    console.log("Error");
+                }}
+                />
+
+                <div className="text-slate-200 font-semibold">
+                    Please sign in with your Google account to continue.
+                </div>
+            </div>
+        </main>
     )
 }
